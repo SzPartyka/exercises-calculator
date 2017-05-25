ecc97b8af9cd8b4a9caba2872b5eb5394c4d029e
Szymon Partyka <sz.partyka@gmail.com>
2017-05-26 00:23:40 +0200

    Update calc-jqery.js

--- kalkulator/calc-jqery.js
+++ kalkulator/calc-jqery.js
@@ -1,38 +1,3 @@
-$('.pies');
-$('#psy');
-$('section');
-$('a[title="malamut"]');
-$('section .psy a');
-$('section').find('a');
-$('section').children();
-$('.pies').next(); // .nextAll, .prev
-$('section div').first(); //.last
-$('a').parents(); // .parent
-
-
-var v = $('input[name="inpoot"]');
-var a = $('.pies a');
-
-$('button').click(function () {
-    var w = v.val();
-    var txtA = a.text();
-    var txtb = a.html();
-    // a.after('<div>Jamnik!</div>')
-    // a.before('<div>Kundel!</div>')
-    console.log(w);
-    // console.log(txtA);
-    //  console.log(txtb);
-
-})
-$('#psy').toggleClass('#error'); // OBCZAIĆ TO I POPRAWIC!
-$('#psy').css('border', "8px solid red");
-
-v.change(function () {
-    console.log($(this).val)
-})
-$('.intext').keyup(function () {
-    $('#psy').text($(this).val);
-})
 $('.show').click(function () {
     v.toggle(500, () => {
         if (v.css('display') == 'none') {
